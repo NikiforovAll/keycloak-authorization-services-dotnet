7d22fc8bc0622d64712007316e5daa45f1885ffc
nikiforovall <alexey.nikiforovall@gmail.com>
2022-01-19 22:40:36 +0200

    docs: add example

--- samples/MinimalApiDependsOnNuGetSource/Program.cs
+++ samples/MinimalApiDependsOnNuGetSource/Program.cs
@@ -1,32 +1,31 @@
-Console.Write("hello world");
-// using Keycloak.AuthServices.Authentication;
-// using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
 
-// var builder = WebApplication.CreateBuilder(args);
-//
-// var host = builder.Host;
-// var configuration = builder.Configuration;
-// var services = builder.Services;
-//
-// host.ConfigureKeycloakConfigurationSource();
-// // conventional registration from keycloak.json
-// services.AddKeycloakAuthentication(configuration);
-//
-// services.AddAuthorization(options =>
-//     {
-//         options.AddPolicy("RequireWorkspaces", builder =>
-//         {
-//             builder.RequireProtectedResource("workspaces", "workspaces:read");
-//         });
-//     })
-//     .AddKeycloakAuthorization(configuration);
-//
-// var app = builder.Build();
-//
-// app.UseAuthentication()
-//     .UseAuthorization();
-//
-// app.MapGet("/workspaces", () => "[]")
-//     .RequireAuthorization("RequireWorkspaces");
-//
-// app.Run();
+var builder = WebApplication.CreateBuilder(args);
+
+var host = builder.Host;
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+host.ConfigureKeycloakConfigurationSource();
+// conventional registration from keycloak.json
+services.AddKeycloakAuthentication(configuration);
+
+services.AddAuthorization(options =>
+    {
+        options.AddPolicy("RequireWorkspaces", builder =>
+        {
+            builder.RequireProtectedResource("workspaces", "workspaces:read");
+        });
+    })
+    .AddKeycloakAuthorization(configuration);
+
+var app = builder.Build();
+
+app.UseAuthentication()
+    .UseAuthorization();
+
+app.MapGet("/workspaces", () => "[]")
+    .RequireAuthorization("RequireWorkspaces");
+
+app.Run();
