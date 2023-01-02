fccc0687be7321e3eb616d7040164598eec1d6e0
Victor Borges <victor1882@outlook.com>
2023-01-02 17:07:29 -0300

    feat: initial user management API (create user)

---
+++ src/Keycloak.AuthServices.Sdk/Admin/IKeycloakUserClient.cs
@@ -0,0 +1,21 @@
+﻿namespace Keycloak.AuthServices.Sdk.Admin;
+
+using Constants;
+using Models;
+using Refit;
+
+/// <summary>
+/// User management
+/// </summary>
+public interface IKeycloakUserClient
+{
+    /// <summary>
+    /// Create a new user
+    /// </summary>
+    /// <param name="realm"></param>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    [Post(KeycloakClientApiConstants.CreateUser)]
+    [Headers("Accept: application/json", "Content-Type: application/json")]
+    Task CreateUser(string realm, [Body] User user);
+}
