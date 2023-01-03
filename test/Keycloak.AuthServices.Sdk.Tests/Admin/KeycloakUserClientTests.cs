a9d42a15029a1780e683092f3a53922f5d21e1af
Victor Borges <victor1882@outlook.com>
2023-01-02 22:14:42 -0300

    test: create user tests

---
+++ test/Keycloak.AuthServices.Sdk.Tests/Admin/KeycloakUserClientTests.cs
@@ -0,0 +1,46 @@
+namespace Keycloak.AuthServices.Sdk.Tests.Admin;
+
+using System.Net;
+using Refit;
+using RichardSzalay.MockHttp;
+using Sdk.Admin;
+using Sdk.Admin.Models;
+
+public class KeycloakUserClientTests
+{
+    private const string BaseAddress = "http://localhost:8080/auth";
+
+    private readonly MockHttpMessageHandler handler = new();
+    private readonly IKeycloakUserClient keycloakUserClient;
+
+    public KeycloakUserClientTests()
+    {
+        var httpClient = this.handler.ToHttpClient();
+        httpClient.BaseAddress = new Uri(BaseAddress);
+
+        this.keycloakUserClient = RestService.For<IKeycloakUserClient>(httpClient);
+    }
+
+    [Fact]
+    public async Task CreateUserShouldCallUserEndpoint()
+    {
+        this.handler.Expect(HttpMethod.Post, $"{BaseAddress}/admin/realms/master/users")
+            .Respond(HttpStatusCode.OK);
+
+        await this.keycloakUserClient.CreateUser("master", new User());
+
+        this.handler.VerifyNoOutstandingExpectation();
+    }
+
+    [Fact]
+    public async Task CreateUserShouldThrowBadRequestApiExceptionWhenRequestIsInvalid()
+    {
+        this.handler.When(HttpMethod.Post, $"{BaseAddress}/admin/realms/master/users")
+            .Respond(HttpStatusCode.BadRequest);
+
+        var exception = await Assert.ThrowsAsync<ApiException>(
+            async () => await this.keycloakUserClient.CreateUser("master", new User()));
+
+        Assert.Equal(HttpStatusCode.BadRequest, exception.StatusCode);
+    }
+}
