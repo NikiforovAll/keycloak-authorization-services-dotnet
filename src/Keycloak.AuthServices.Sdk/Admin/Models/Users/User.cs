bdaa51bd86ecac9849f33d7fb11f53f56966e62a
Victor Borges <victor1882@outlook.com>
2023-01-13 14:38:21 -0300

    Add GetUsers and UpdateUser endpoints

--- src/Keycloak.AuthServices.Sdk/Admin/Models/Users/User.cs
+++ src/Keycloak.AuthServices.Sdk/Admin/Models/Users/User.cs
@@ -6,28 +6,28 @@ namespace Keycloak.AuthServices.Sdk.Admin.Models;
 /// </summary>
 public class User
 {
-    public Dictionary<string, bool>? Access { get; set; }
-    public Dictionary<string, string[]>? Attributes { get; set; }
-    public UserConsent[]? ClientConsents { get; set; }
-    public Dictionary<string, string>? ClientRoles { get; set; }
-    public long? CreatedTimestamp { get; set; }
-    public Credential[]? Credentials { get; set; }
-    public string[]? DisableableCredentialTypes { get; set; }
-    public string? Email { get; set; }
-    public bool? EmailVerified { get; set; }
-    public bool? Enabled { get; set; }
-    public FederatedIdentity[]? FederatedIdentities { get; set; }
-    public string? FederationLink { get; set; }
-    public string? FirstName { get; set; }
-    public string[]? Groups { get; set; }
-    public string? Id { get; set; }
-    public string? LastName { get; set; }
-    public int? NotBefore { get; set; }
-    public string? Origin { get; set; }
-    public string[]? RealmRoles { get; set; }
-    public string[]? RequiredActions { get; set; }
-    public string? Self { get; set; }
-    public string? ServiceAccountClientId { get; set; }
-    public string? Username { get; set; }
-    public bool? Totp { get; set; }
+    public Dictionary<string, bool>? Access { get; init; }
+    public Dictionary<string, string[]>? Attributes { get; init; }
+    public UserConsent[]? ClientConsents { get; init; }
+    public Dictionary<string, string>? ClientRoles { get; init; }
+    public long? CreatedTimestamp { get; init; }
+    public Credential[]? Credentials { get; init; }
+    public string[]? DisableableCredentialTypes { get; init; }
+    public string? Email { get; init; }
+    public bool? EmailVerified { get; init; }
+    public bool? Enabled { get; init; }
+    public FederatedIdentity[]? FederatedIdentities { get; init; }
+    public string? FederationLink { get; init; }
+    public string? FirstName { get; init; }
+    public string[]? Groups { get; init; }
+    public string? Id { get; init; }
+    public string? LastName { get; init; }
+    public int? NotBefore { get; init; }
+    public string? Origin { get; init; }
+    public string[]? RealmRoles { get; init; }
+    public string[]? RequiredActions { get; init; }
+    public string? Self { get; init; }
+    public string? ServiceAccountClientId { get; init; }
+    public string? Username { get; init; }
+    public bool? Totp { get; init; }
 }
