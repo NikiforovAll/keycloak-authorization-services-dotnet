0950094716d5f3268b9900b7f459cd27600a27d4
Jim Hume <jim@ethor.com>
2023-05-09 06:39:27 -0700

    Get the groups of a user
    The user API doesn't return groups when getting a list of users. This allows for the explicit call to get the groups of a single user.

--- src/Keycloak.AuthServices.Sdk/Admin/Constants/KeycloakClientApiConstants.cs
+++ src/Keycloak.AuthServices.Sdk/Admin/Constants/KeycloakClientApiConstants.cs
@@ -1,88 +1,90 @@
-namespace Keycloak.AuthServices.Sdk.Admin.Constants;
-
-/// <summary>
-/// Keycloak API endpoints
-/// </summary>
-internal static class KeycloakClientApiConstants
-{
-    private const string AdminApiBase = "/admin";
-
-    private const string Realms = "realms";
-
-    internal const string GetRealm = $"{AdminApiBase}/{Realms}/{{realm}}";
-
-
-    #region Resource API
-
-    internal const string GetResources = "/realms/{realm}/authz/protection/resource_set";
-
-    internal const string GetResource = $"{GetResources}/{{id}}";
-
-    internal const string CreateResource = $"{GetResources}";
-
-    internal const string PutResource = $"{GetResource}";
-
-    internal const string DeleteResource = $"{GetResource}";
-
-    internal const string GetResourceByExactName = "/realms/{realm}/authz/protection/resource_set?&exactName=true";
-
-    #endregion
-
-    #region User API
-
-    internal const string GetUsers = $"{GetRealm}/users";
-
-    internal const string GetUser = $"{GetRealm}/users/{{id}}";
-
-    internal const string CreateUser = $"{GetRealm}/users";
-
-    internal const string UpdateUser = $"{GetRealm}/users/{{id}}";
-
-    internal const string SendVerifyEmail = $"{GetRealm}/users/{{id}}/send-verify-email";
-
-    internal const string UserGroupUpdate = $"{UpdateUser}/groups/{{group_id}}";
-
-    #endregion
-
-    #region Group API
-
-    internal const string GetGroups = $"{GetRealm}/groups";
-
-    internal const string GetGroup = $"{GetRealm}/groups/{{id}}";
-
-    internal const string CreateGroup = $"{GetGroups}";
-
-    internal const string UpdateGroup = $"{GetGroup}";
-
-    internal const string DeleteGroup = $"{GetGroup}";
-
-    internal const string CreateChildGroup = $"{GetGroup}/children";
-
-    #endregion
-
-    #region Permission Ticket API
-
-    internal const string GetPermissionTickets = "/realms/{realm}/authz/protection/permission/ticket";
-
-    internal const string CreatePermissionTicket = $"{GetPermissionTickets}";
-
-    internal const string UpdatePermissionTicket = $"{GetPermissionTickets}";
-
-    internal const string DeletePermissionTicket = $"{GetPermissionTickets}/{{id}}";
-
-    #endregion
-
-    #region Policy API
-
-    internal const string GetPolicies = "/realms/{realm}/authz/protection/uma-policy";
-
-    internal const string GetPolicy = $"{GetPolicies}/{{id}}";
-
-    internal const string CreatePolicy = $"{GetPolicy}";
-
-    internal const string UpdatePolicy = $"{GetPolicy}";
-
-    internal const string DeletePolicy = $"{GetPolicy}";
-
-    #endregion
-}
+namespace Keycloak.AuthServices.Sdk.Admin.Constants; 
+ 
+/// <summary> 
+/// Keycloak API endpoints 
+/// </summary> 
+internal static class KeycloakClientApiConstants 
+{ 
+    private const string AdminApiBase = "/admin"; 
+ 
+    private const string Realms = "realms"; 
+ 
+    internal const string GetRealm = $"{AdminApiBase}/{Realms}/{{realm}}"; 
+ 
+ 
+    #region Resource API 
+ 
+    internal const string GetResources = "/realms/{realm}/authz/protection/resource_set"; 
+ 
+    internal const string GetResource = $"{GetResources}/{{id}}"; 
+ 
+    internal const string CreateResource = $"{GetResources}"; 
+ 
+    internal const string PutResource = $"{GetResource}"; 
+ 
+    internal const string DeleteResource = $"{GetResource}"; 
+ 
+    internal const string GetResourceByExactName = "/realms/{realm}/authz/protection/resource_set?&exactName=true"; 
+ 
+    #endregion 
+ 
+    #region User API 
+ 
+    internal const string GetUsers = $"{GetRealm}/users"; 
+ 
+    internal const string GetUser = $"{GetRealm}/users/{{id}}"; 
+ 
+    internal const string CreateUser = $"{GetRealm}/users"; 
+ 
+    internal const string UpdateUser = $"{GetRealm}/users/{{id}}"; 
+ 
+    internal const string SendVerifyEmail = $"{GetRealm}/users/{{id}}/send-verify-email"; 
+ 
+    internal const string GetUserGroups = $"{UpdateUser}/groups"; 
+ 
+    internal const string UserGroupUpdate = $"{GetUserGroups}/{{group_id}}"; 
+ 
+    #endregion 
+ 
+    #region Group API 
+ 
+    internal const string GetGroups = $"{GetRealm}/groups"; 
+ 
+    internal const string GetGroup = $"{GetRealm}/groups/{{id}}"; 
+ 
+    internal const string CreateGroup = $"{GetGroups}"; 
+ 
+    internal const string UpdateGroup = $"{GetGroup}"; 
+ 
+    internal const string DeleteGroup = $"{GetGroup}"; 
+ 
+    internal const string CreateChildGroup = $"{GetGroup}/children"; 
+ 
+    #endregion 
+ 
+    #region Permission Ticket API 
+ 
+    internal const string GetPermissionTickets = "/realms/{realm}/authz/protection/permission/ticket"; 
+ 
+    internal const string CreatePermissionTicket = $"{GetPermissionTickets}"; 
+ 
+    internal const string UpdatePermissionTicket = $"{GetPermissionTickets}"; 
+ 
+    internal const string DeletePermissionTicket = $"{GetPermissionTickets}/{{id}}"; 
+ 
+    #endregion 
+ 
+    #region Policy API 
+ 
+    internal const string GetPolicies = "/realms/{realm}/authz/protection/uma-policy"; 
+ 
+    internal const string GetPolicy = $"{GetPolicies}/{{id}}"; 
+ 
+    internal const string CreatePolicy = $"{GetPolicy}"; 
+ 
+    internal const string UpdatePolicy = $"{GetPolicy}"; 
+ 
+    internal const string DeletePolicy = $"{GetPolicy}"; 
+ 
+    #endregion 
+} 
--- src/Keycloak.AuthServices.Sdk/Admin/IKeycloakUserClient.cs
+++ src/Keycloak.AuthServices.Sdk/Admin/IKeycloakUserClient.cs
@@ -1,93 +1,104 @@
-namespace Keycloak.AuthServices.Sdk.Admin;
-
-using Constants;
-using Models.Users;
-using Refit;
-using Requests.Users;
-
-/// <summary>
-/// User management
-/// </summary>
-[Headers("Accept: application/json")]
-public interface IKeycloakUserClient
-{
-    /// <summary>
-    /// Get a stream of users on the realm.
-    /// </summary>
-    /// <param name="realm">Realm name (not ID).</param>
-    /// <param name="parameters">Optional query parameters.</param>
-    /// <returns>A stream of users, filtered according to query parameters.</returns>
-    [Get(KeycloakClientApiConstants.GetUsers)]
-    Task<IEnumerable<User>> GetUsers(string realm, [Query] GetUsersRequestParameters? parameters = default);
-
-    /// <summary>
-    /// Get representation of a user.
-    /// </summary>
-    /// <param name="realm">Realm name (not ID).</param>
-    /// <param name="userId">User ID.</param>
-    /// <returns>The user representation.</returns>
-    [Get(KeycloakClientApiConstants.GetUser)]
-    Task<User> GetUser(string realm, [AliasAs("id")] string userId);
-
-    /// <summary>
-    /// Create a new user.
-    /// </summary>
-    /// <remarks>
-    /// Username must be unique.
-    /// </remarks>
-    /// <param name="realm">Realm name (not ID).</param>
-    /// <param name="user">User representation.</param>
-    /// <returns></returns>
-    [Post(KeycloakClientApiConstants.CreateUser)]
-    [Headers("Content-Type: application/json")]
-    Task<HttpResponseMessage> CreateUser(string realm, [Body] User user);
-
-    /// <summary>
-    /// Update the user.
-    /// </summary>
-    /// <param name="realm">Realm name (not ID).</param>
-    /// <param name="userId">User ID.</param>
-    /// <param name="user">User representation.</param>
-    /// <returns></returns>
-    [Put(KeycloakClientApiConstants.UpdateUser)]
-    [Headers("Content-Type: application/json")]
-    Task UpdateUser(string realm, [AliasAs("id")] string userId, [Body] User user);
-
-    /// <summary>
-    /// Send an email-verification email to the user.
-    /// </summary>
-    /// <remarks>
-    /// An email contains a link the user can click to verify their email address.
-    /// </remarks>
-    /// <param name="realm">Realm name (not ID).</param>
-    /// <param name="userId">User ID.</param>
-    /// <param name="clientId">Client ID.</param>
-    /// <param name="redirectUri">Redirect URI. The default for the redirect is the account client.</param>
-    [Put(KeycloakClientApiConstants.SendVerifyEmail)]
-    Task SendVerifyEmail(string realm, [AliasAs("id")] string userId,
-        [Query][AliasAs("client_id")] string? clientId = default,
-        [Query][AliasAs("redirect_uri")] string? redirectUri = default);
-
-
-    /// <summary>
-    /// Add the given user to the given group.
-    /// </summary>
-    /// <param name="realm"></param>
-    /// <param name="userId"></param>
-    /// <param name="groupId"></param>
-    /// <returns></returns>
-    [Put(KeycloakClientApiConstants.UserGroupUpdate)]
-    [Headers("Content-Type: application/json")]
-    Task JoinGroup(string realm, [AliasAs("id")] string userId, [AliasAs("group_id")] string groupId);
-
-    /// <summary>
-    /// Add the given user to the given group.
-    /// </summary>
-    /// <param name="realm"></param>
-    /// <param name="userId"></param>
-    /// <param name="groupId"></param>
-    /// <returns></returns>
-    [Delete(KeycloakClientApiConstants.UserGroupUpdate)]
-    [Headers("Content-Type: application/json")]
-    Task LeaveGroup(string realm, [AliasAs("id")] string userId, [AliasAs("group_id")] string groupId);
-}
+using Keycloak.AuthServices.Sdk.Admin.Models.Groups; 
+ 
+namespace Keycloak.AuthServices.Sdk.Admin; 
+ 
+using Constants; 
+using Models.Users; 
+using Refit; 
+using Requests.Users; 
+ 
+/// <summary> 
+/// User management 
+/// </summary> 
+[Headers("Accept: application/json")] 
+public interface IKeycloakUserClient 
+{ 
+    /// <summary> 
+    /// Get a stream of users on the realm. 
+    /// </summary> 
+    /// <param name="realm">Realm name (not ID).</param> 
+    /// <param name="parameters">Optional query parameters.</param> 
+    /// <returns>A stream of users, filtered according to query parameters.</returns> 
+    [Get(KeycloakClientApiConstants.GetUsers)] 
+    Task<IEnumerable<User>> GetUsers(string realm, [Query] GetUsersRequestParameters? parameters = default); 
+ 
+    /// <summary> 
+    /// Get representation of a user. 
+    /// </summary> 
+    /// <param name="realm">Realm name (not ID).</param> 
+    /// <param name="userId">User ID.</param> 
+    /// <returns>The user representation.</returns> 
+    [Get(KeycloakClientApiConstants.GetUser)] 
+    Task<User> GetUser(string realm, [AliasAs("id")] string userId); 
+ 
+    /// <summary> 
+    /// Create a new user. 
+    /// </summary> 
+    /// <remarks> 
+    /// Username must be unique. 
+    /// </remarks> 
+    /// <param name="realm">Realm name (not ID).</param> 
+    /// <param name="user">User representation.</param> 
+    /// <returns></returns> 
+    [Post(KeycloakClientApiConstants.CreateUser)] 
+    [Headers("Content-Type: application/json")] 
+    Task<HttpResponseMessage> CreateUser(string realm, [Body] User user); 
+ 
+    /// <summary> 
+    /// Update the user. 
+    /// </summary> 
+    /// <param name="realm">Realm name (not ID).</param> 
+    /// <param name="userId">User ID.</param> 
+    /// <param name="user">User representation.</param> 
+    /// <returns></returns> 
+    [Put(KeycloakClientApiConstants.UpdateUser)] 
+    [Headers("Content-Type: application/json")] 
+    Task UpdateUser(string realm, [AliasAs("id")] string userId, [Body] User user); 
+ 
+    /// <summary> 
+    /// Send an email-verification email to the user. 
+    /// </summary> 
+    /// <remarks> 
+    /// An email contains a link the user can click to verify their email address. 
+    /// </remarks> 
+    /// <param name="realm">Realm name (not ID).</param> 
+    /// <param name="userId">User ID.</param> 
+    /// <param name="clientId">Client ID.</param> 
+    /// <param name="redirectUri">Redirect URI. The default for the redirect is the account client.</param> 
+    [Put(KeycloakClientApiConstants.SendVerifyEmail)] 
+    Task SendVerifyEmail(string realm, [AliasAs("id")] string userId, 
+        [Query][AliasAs("client_id")] string? clientId = default, 
+        [Query][AliasAs("redirect_uri")] string? redirectUri = default); 
+ 
+ 
+    /// <summary> 
+    /// Add the given user to the given group. 
+    /// </summary> 
+    /// <param name="realm"></param> 
+    /// <param name="userId"></param> 
+    /// <param name="groupId"></param> 
+    /// <returns></returns> 
+    [Put(KeycloakClientApiConstants.UserGroupUpdate)] 
+    [Headers("Content-Type: application/json")] 
+    Task JoinGroup(string realm, [AliasAs("id")] string userId, [AliasAs("group_id")] string groupId); 
+ 
+    /// <summary> 
+    /// Remove the given user from the given group. 
+    /// </summary> 
+    /// <param name="realm"></param> 
+    /// <param name="userId"></param> 
+    /// <param name="groupId"></param> 
+    /// <returns></returns> 
+    [Delete(KeycloakClientApiConstants.UserGroupUpdate)] 
+    [Headers("Content-Type: application/json")] 
+    Task LeaveGroup(string realm, [AliasAs("id")] string userId, [AliasAs("group_id")] string groupId); 
+ 
+    /// <summary> 
+    /// Get all the groups the user belongs to. 
+    /// </summary> 
+    /// <param name="realm"></param> 
+    /// <param name="userId"></param> 
+    /// <returns></returns> 
+    [Get(KeycloakClientApiConstants.GetUserGroups)] 
+    Task<IEnumerable<Group>> GetUserGroups(string realm, [AliasAs("id")] string userId); 
+} 
