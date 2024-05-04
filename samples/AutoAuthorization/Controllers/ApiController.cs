b47b42445de48b41b17c08d67d5af578ce41377e
Friedemann Braune <friedemann.braune@gmail.comn>
2024-05-04 22:05:23 +0200

    introduce DisableAutoProtection and ExplicitResourceProtection; Refactor Middleware; Extend example app

--- samples/AutoAuthorization/Controllers/ApiController.cs
+++ samples/AutoAuthorization/Controllers/ApiController.cs
@@ -1,4 +1,6 @@
 namespace AutoAuthorization.Controllers;
+
+using Keycloak.AuthServices.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
@@ -8,4 +10,16 @@ public class ApiController : ControllerBase
     [Route("/api/auto-auth")]
     [ProducesResponseType(statusCode: 200, type: typeof(string))]
     public IActionResult GetData() => this.Ok("Auto Authorization works.");
+
+    [HttpGet]
+    [Route("/api/no-auth")]
+    [ProducesResponseType(statusCode: 200, type: typeof(string))]
+    [DisableUriBasedResourceProtection]
+    public IActionResult GetDataWithoutAuth() => this.Ok("Auto Authorization works, but is disabled by attribute.");
+
+    [HttpGet]
+    [Route("/api/explicit-auth")]
+    [ProducesResponseType(statusCode: 200, type: typeof(string))]
+    [ExplicitResourceProtection("/api/auto-auth", "GET")]
+    public IActionResult GetDataWithExplicitAuth() => this.Ok("Auto Authorization works with explicit choosing resource name and scope.");
 }
