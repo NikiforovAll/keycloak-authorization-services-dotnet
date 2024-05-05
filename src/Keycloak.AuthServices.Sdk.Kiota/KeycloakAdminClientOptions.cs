a81762274556bafbd7faea3d38435fe24f8886a4
nikiforovall <alexey.nikiforovall@gmail.com>
2024-05-05 11:44:45 +0300

    Add KeycloakAdminApiClient

---
+++ src/Keycloak.AuthServices.Sdk.Kiota/KeycloakAdminClientOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Keycloak.AuthServices.Sdk.Kiota;
+
+using Common;
+
+/// <summary>
+/// Defines a set of options used to perform Admin HTTP Client calls
+/// </summary>
+public sealed class KeycloakAdminClientOptions : KeycloakInstallationOptions
+{
+    /// <summary>
+    /// Default section name
+    /// </summary>
+    public const string Section = ConfigurationConstants.ConfigurationPrefix;
+}
