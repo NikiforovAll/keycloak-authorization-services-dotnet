88a5895d0a4c7b3174c3b5eb4a74bc20d59faf93
Alex <alexredding180@gmail.com>
2024-05-10 00:50:39 +0100

    fix: Remove early returns in claims transformation will allow it check all flags

--- src/Keycloak.AuthServices.Authorization/Claims/KeycloakRolesClaimsTransformation.cs
+++ src/Keycloak.AuthServices.Authorization/Claims/KeycloakRolesClaimsTransformation.cs
@@ -73,38 +73,34 @@ public class KeycloakRolesClaimsTransformation : IClaimsTransformation
         if (this.roleSource.HasFlag(RolesClaimTransformationSource.ResourceAccess))
         {
             var resourceAccessValue = principal.FindFirst("resource_access")?.Value;
-            if (string.IsNullOrWhiteSpace(resourceAccessValue))
+            if (!string.IsNullOrWhiteSpace(resourceAccessValue))
             {
-                return Task.FromResult(result);
-            }
-
-            using var resourceAccess = JsonDocument.Parse(resourceAccessValue);
-            var containsAudienceRoles = resourceAccess.RootElement.TryGetProperty(
-                this.audience,
-                out var rolesElement
-            );
-
-            if (!containsAudienceRoles)
-            {
-                return Task.FromResult(result);
-            }
-
-            var clientRoles = rolesElement.GetProperty("roles");
-
-            foreach (var role in clientRoles.EnumerateArray())
-            {
-                var value = role.GetString();
-
-                var matchingClaim = identity.Claims.FirstOrDefault(claim =>
-                    claim.Type.Equals(
-                        this.roleClaimType,
-                        StringComparison.InvariantCultureIgnoreCase
-                    ) && claim.Value.Equals(value, StringComparison.InvariantCultureIgnoreCase)
+                using var resourceAccess = JsonDocument.Parse(resourceAccessValue);
+                var containsAudienceRoles = resourceAccess.RootElement.TryGetProperty(
+                    this.audience,
+                    out var rolesElement
                 );
 
-                if (matchingClaim is null && !string.IsNullOrWhiteSpace(value))
+                if (containsAudienceRoles)
                 {
-                    identity.AddClaim(new Claim(this.roleClaimType, value));
+                    var clientRoles = rolesElement.GetProperty("roles");
+
+                    foreach (var role in clientRoles.EnumerateArray())
+                    {
+                        var value = role.GetString();
+
+                        var matchingClaim = identity.Claims.FirstOrDefault(claim =>
+                            claim.Type.Equals(
+                                this.roleClaimType,
+                                StringComparison.InvariantCultureIgnoreCase
+                            ) && claim.Value.Equals(value, StringComparison.InvariantCultureIgnoreCase)
+                        );
+
+                        if (matchingClaim is null && !string.IsNullOrWhiteSpace(value))
+                        {
+                            identity.AddClaim(new Claim(this.roleClaimType, value));
+                        }
+                    }
                 }
             }
         }
@@ -112,34 +108,32 @@ public class KeycloakRolesClaimsTransformation : IClaimsTransformation
         if (this.roleSource.HasFlag(RolesClaimTransformationSource.Realm))
         {
             var realmAccessValue = principal.FindFirst("realm_access")?.Value;
-            if (string.IsNullOrWhiteSpace(realmAccessValue))
+            if (!string.IsNullOrWhiteSpace(realmAccessValue))
             {
-                return Task.FromResult(result);
-            }
+                using var realmAccess = JsonDocument.Parse(realmAccessValue);
 
-            using var realmAccess = JsonDocument.Parse(realmAccessValue);
+                var containsRoles = realmAccess.RootElement.TryGetProperty(
+                    "roles",
+                    out var rolesElement
+                );
 
-            var containsRoles = realmAccess.RootElement.TryGetProperty(
-                "roles",
-                out var rolesElement
-            );
-
-            if (containsRoles)
-            {
-                foreach (var role in rolesElement.EnumerateArray())
+                if (containsRoles)
                 {
-                    var value = role.GetString();
-
-                    var matchingClaim = identity.Claims.FirstOrDefault(claim =>
-                        claim.Type.Equals(
-                            this.roleClaimType,
-                            StringComparison.InvariantCultureIgnoreCase
-                        ) && claim.Value.Equals(value, StringComparison.InvariantCultureIgnoreCase)
-                    );
-
-                    if (matchingClaim is null && !string.IsNullOrWhiteSpace(value))
+                    foreach (var role in rolesElement.EnumerateArray())
                     {
-                        identity.AddClaim(new Claim(this.roleClaimType, value));
+                        var value = role.GetString();
+
+                        var matchingClaim = identity.Claims.FirstOrDefault(claim =>
+                            claim.Type.Equals(
+                                this.roleClaimType,
+                                StringComparison.InvariantCultureIgnoreCase
+                            ) && claim.Value.Equals(value, StringComparison.InvariantCultureIgnoreCase)
+                        );
+
+                        if (matchingClaim is null && !string.IsNullOrWhiteSpace(value))
+                        {
+                            identity.AddClaim(new Claim(this.roleClaimType, value));
+                        }
                     }
                 }
             }
