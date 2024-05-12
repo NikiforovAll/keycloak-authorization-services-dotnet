92f8dfd1aca712c0ffa5357d7032432f52b551aa
nikiforovall <alexey.nikiforovall@gmail.com>
2024-05-12 12:24:20 +0300

    feat: Add RolesAccessTokenMapping for WebApp scenario

--- docs/examples/web-app-mvc.md
+++ docs/examples/web-app-mvc.md
@@ -1,5 +1,14 @@
 # WebApp MVC
 
+<!--@include: @/../samples/WebApp/README.md-->
+
+### Role Mapping
+
+> [!WARNING]
+> By default Keycloak doesn't map roles to **id_token**, so we need an **access_token** in this case, **access_token** is **NOT** available in all OAuth flows. For example, "Implicit Flow" is based on id_token and **access_token** is not retrieved at all.
+
+## Code
+
 <<< @/../samples/WebApp/Program.cs
 
 <<< @/../samples/WebApp/Controllers/AccountController.cs
--- samples/WebApp/Controllers/AccountController.cs
+++ samples/WebApp/Controllers/AccountController.cs
@@ -9,9 +9,14 @@ using Microsoft.AspNetCore.Mvc;
 
 public class AccountController : Controller
 {
+    private readonly ILogger<AccountController> logger;
+
+    public AccountController(ILogger<AccountController> logger) => this.logger = logger;
+
+    [AllowAnonymous]
     public IActionResult SignIn()
     {
-        if (!User.Identity.IsAuthenticated)
+        if (!this.User.Identity!.IsAuthenticated)
         {
             return this.Challenge(OpenIdConnectDefaults.AuthenticationScheme);
         }
@@ -19,10 +24,25 @@ public class AccountController : Controller
         return this.RedirectToAction("Index", "Home");
     }
 
-    [Authorize]
+    [AllowAnonymous]
     public async Task<IActionResult> SignOutAsync()
     {
+        if (!this.User.Identity!.IsAuthenticated)
+        {
+            return this.Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
         var idToken = await this.HttpContext.GetTokenAsync("id_token");
+
+        var authResult = this
+            .HttpContext.Features.Get<IAuthenticateResultFeature>()
+            ?.AuthenticateResult;
+
+        var tokens = authResult!.Properties!.GetTokens();
+
+        var tokenNames = tokens.Select(token => token.Name).ToArray();
+
+        this.logger.LogInformation("Token Names: {TokenNames}", string.Join(", ", tokenNames));
 
         return this.SignOut(
             new AuthenticationProperties
@@ -34,4 +54,7 @@ public class AccountController : Controller
             OpenIdConnectDefaults.AuthenticationScheme
         );
     }
+
+    [AllowAnonymous]
+    public IActionResult AccessDenied() => this.RedirectToAction("AccessDenied", "Home");
 }
