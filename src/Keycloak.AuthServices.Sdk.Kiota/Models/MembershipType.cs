f79a24f4ba1a36bfd8781192aec80a80a1ed25ff
Jean-Baptiste Zeller <esurnir@gmail.com>
2025-03-24 20:09:59 +0100

    feat: regenerate kiota client using version 26.1.3

--- src/Keycloak.AuthServices.Sdk.Kiota/Models/MembershipType.cs
+++ src/Keycloak.AuthServices.Sdk.Kiota/Models/MembershipType.cs
@@ -3,6 +3,7 @@ using System.Runtime.Serialization;
 using System;
 namespace Keycloak.AuthServices.Sdk.Kiota.Admin.Models
 {
+    [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
     public enum MembershipType
     #pragma warning restore CS1591
