8fde517ca0630ed1c9ca25af39d798782d218cf7
Lucas Almeida <46822883+lucas-pollus@users.noreply.github.com>
2025-08-25 16:35:54 -0300

    feat: add user's credentials manager (#177)

--- src/Keycloak.AuthServices.Sdk/Admin/ApiUrls.cs
+++ src/Keycloak.AuthServices.Sdk/Admin/ApiUrls.cs
@@ -35,6 +35,13 @@ internal static class ApiUrls
     internal const string JoinGroup = $"{GetRealm}/users/{{id}}/groups/{{groupId}}";
 
     internal const string LeaveGroup = $"{GetRealm}/users/{{id}}/groups/{{groupId}}";
+
+    internal const string ResetPassword = $"{GetRealm}/users/{{id}}/reset-password";
+
+    internal const string DeleteCredential = $"{GetRealm}/users/{{id}}/credentials/{{credentialId}}";
+
+    internal const string GetCredentials = $"{GetRealm}/users/{{id}}/credentials";
+
     #endregion
 
     #region Group API
